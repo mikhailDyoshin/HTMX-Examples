463f3e68ec9b3dfa19becd9453b903e710434ecf
Дёшин Михаил <m.deshin@vniias.ru>
2026-07-07 14:51:17 +0300

    Refactored the navigation
    Now active-nav-link-logic is inside a template

--- app/bp/navigation.py
+++ app/bp/navigation.py
@@ -1,123 +1,68 @@
-from typing import Any
-
-from flask import Blueprint, Request, render_template, request
+from flask import Blueprint, render_template, request
 from dataclasses import dataclass
-from enum import Enum
 
 from flask.typing import RouteCallable
 
 
 bp = Blueprint("navigation", __name__, url_prefix="")
 
-PARTIALS_DIR = "/partials"
-NAVIGATION_TEMPLATE = f"{PARTIALS_DIR}/side_bar.html"
-
-
-@dataclass(frozen=True)
-class NavigationLink:
-    url: str
-    text: str
-    style: str
-
-
-@dataclass(frozen=True)
-class NavigationRoute:
-    url: str
-    link_text: str
-
-
-@dataclass(frozen=True)
-class Partial:
-    template: str
-    data: Any
-
 
 @dataclass(frozen=True)
 class Page:
-    navigation: Partial
-    content: Partial
+    url: str
+    title: str
+    template: str
+    context: object = None
 
 
-def partial_template(name: str) -> str:
-    return f"partials/{name}.html"
-
-
-class Pages(Enum):
-    HOME = Page(
-        navigation=Partial(NAVIGATION_TEMPLATE, NavigationRoute("/", "Home")),
-        content=Partial(partial_template("home"), None),
-    )
-    SETTINGS = Page(
-        navigation=Partial(
-            NAVIGATION_TEMPLATE,
-            NavigationRoute("/settings", "Settings"),
-        ),
-        content=Partial(partial_template("settings"), None),
-    )
-
-
-NAVIGATION_ROUTES: tuple[NavigationRoute, ...] = tuple(
-    [p.value.navigation.data for p in Pages]
+PAGES = (
+    Page("/", "Home", "partials/home.html"),
+    Page("/settings", "Settings", "partials/settings.html"),
 )
 
 
-def is_hx_request(request: Request) -> bool:
+def is_hx_request() -> bool:
     return bool(request.headers.get("HX-Request"))
 
 
-def nav_link(
-    current_item: NavigationRoute, selected_item: NavigationRoute
-) -> NavigationLink:
-    style = "active" if selected_item is current_item else ""
-    return NavigationLink(
-        url=current_item.url,
-        text=current_item.link_text,
-        style=" ".join(("", style)).strip(),
-    )
-
-
-def navigation_links(active: NavigationRoute) -> tuple[NavigationLink, ...]:
-    return tuple([nav_link(i, active) for i in NAVIGATION_ROUTES])
-
-
-def navigation_oob_template(active_menu_item: NavigationRoute) -> str:
+def navigation_oob_template(active_page: Page) -> str:
     return render_template(
-        "/partials/navigation_oob.html",
-        navigation_data=navigation_links(active_menu_item),
+        "/partials/navigation_oob.html", pages=PAGES, active_page=active_page
     )
 
 
-def render_page(page: Pages) -> str:
+def render_page(page: Page) -> str:
     return render_template(
         "/page.html",
-        content_template=page.value.content.template,
-        content_data=page.value.content.data,
-        navigation_data=navigation_links(page.value.navigation.data),
+        content_template=page.template,
+        content_data=page.context,
+        pages=PAGES,
+        active_page=page,
     )
 
 
-def render_partial(page: Pages) -> str:
-    return render_template(
-        page.value.content.template, data=page.value.content.data
-    ) + navigation_oob_template(page.value.navigation.data)
+def render_partial(page: Page) -> str:
+    return render_template(page.template, data=page.context) + navigation_oob_template(
+        page
+    )
 
 
-def render(request: Request, page: Pages) -> str:
-    if is_hx_request(request):
+def render(page: Page) -> str:
+    if is_hx_request():
         return render_partial(page)
     return render_page(page)
 
 
-def page_view(page: Pages) -> RouteCallable:
+def page_view(page: Page) -> RouteCallable:
     def view():
-        return render(request, page)
+        return render(page)
 
     return view
 
 
-for p in Pages:
+for p in PAGES:
     bp.add_url_rule(
-        rule=p.value.navigation.data.url,
-        endpoint=p.name.lower(),
+        rule=p.url,
+        endpoint=p.title.lower(),
         view_func=page_view(p),
     )
